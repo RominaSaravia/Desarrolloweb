9cf0989a234c872b04fb1789c6e4c6d7cbe29c53
RominaSaravia <rominalorenasaravia@gmail.com>
2024-05-24 20:22:15 -0300

    2 Triggers nuevos: insert nueva ficha_vacuna con su visita y su validacion antes del insert.

--- EjerciciosSQL/TP/Creation.sql
+++ EjerciciosSQL/TP/Creation.sql
@@ -65,7 +65,8 @@ create Table visitas(
 );
 
 
-CREATE FUNCTION trigger_new_animal() 
+------------------------CREACION_NUEVA_FICHA_VACUNA---------------------------------
+CREATE OR REPLACE FUNCTION trigger_new_animal() 
 RETURNS trigger
 AS $$
 BEGIN
@@ -82,5 +83,69 @@ AFTER INSERT
 ON animales
 FOR EACH ROW
 EXECUTE FUNCTION trigger_new_animal();
+
+
+------------------------CREACION_NUEVA_FICHA_VACUNA---------------------------------
+
+CREATE OR REPLACE FUNCTION trigger_new_ficha_vacuna() 
+RETURNS trigger AS $$
+DECLARE nombre_vacuna VARCHAR(200);
+DECLARE costo_vacuna NUMERIC(11,2);
+BEGIN
+SELECT nombre, coste into nombre_vacuna, costo_vacuna from vacunas Where id_vacuna = NEW.vacuna;
+    INSERT into visitas(ficha_medica,consulta,fecha,costo_visita) 
+    values(NEW.ficha_medica,concat('Apliacion vacuna: ',nombre_vacuna), current_timestamp, costo_vacuna);
+   
+    return NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_new_ficha_vacuna_visita
+AFTER INSERT
+ON fichas_vacunas
+FOR EACH ROW
+EXECUTE FUNCTION trigger_new_ficha_vacuna();
+
+
+---------------------VALIDACION_NUEVA_VACUNA----------------------------
+CREATE OR REPLACE FUNCTION trigger_new_ficha_vacuna_val() 
+RETURNS trigger AS $$
+DECLARE nInterval integer;
+DECLARE last_application TIMESTAMP;
+DECLARE timeFlag TIMESTAMP;
+BEGIN
+    last_application := (SELECT fecha_aplicacion from fichas_vacunas Where vacuna = NEW.vacuna AND ficha_medica = NEW.ficha_medica ORDER BY fecha_aplicacion DESC LIMIT 1);
+    nInterval := (SELECT duracion_meses from vacunas Where id_vacuna = NEW.vacuna  LIMIT 1);
+    timeFlag = last_application + nInterval *  INTERVAL '1 MONTH';
+    if (   (timeFlag   < NEW.fecha_aplicacion AND nInterval <> 99 ) OR last_application IS NULL) 
+    THEN
+    return NEW;
+    END IF;
+    if(nInterval <> 99) THEN
+    RAISE EXCEPTION '--> Última aplicación fue en: %  proxima fecha para la vacuna: %', to_char(last_application, 'YYYY-MM-DD') , to_char(timeFlag, 'YYYY-MM-DD') ;
+    END IF;
+    RAISE EXCEPTION 'Esta vacuna ya fue aplicada el %', last_application;
+
+    return NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_ficha_vacuna_val
+BEFORE INSERT
+ON fichas_vacunas
+FOR EACH ROW
+EXECUTE FUNCTION trigger_new_ficha_vacuna_val();
+
+------------------------------------------------------------------------
+
+----TEST_VALIDACION_NUEVA_FICHA_VACUNA-------
+insert into fichas_vacunas (ficha_medica,vacuna,fecha_aplicacion)
+values (1,5, current_timestamp + INTERVAL '12 MONTH');
+
+insert into fichas_vacunas (ficha_medica,vacuna,fecha_aplicacion)
+values (1,5, current_timestamp);
+
+insert into fichas_vacunas (ficha_medica,vacuna,fecha_aplicacion)
+values (2,1, current_timestamp);
 
 
