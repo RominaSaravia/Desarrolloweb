57dcc67253160fe1ff653f44a1136be0b03623d9
RominaSaravia <61885187+RominaSaravia@users.noreply.github.com>
2024-06-06 20:13:14 -0300

    Update ejerciciosResueltos.sql Ejercicio 7 y 8

--- EjerciciosSQL/TP/ejerciciosResueltos.sql
+++ EjerciciosSQL/TP/ejerciciosResueltos.sql
@@ -71,3 +71,46 @@ LEFT JOIN vacunas ON fichas_vacunas.vacuna = vacunas.id_vacuna
 GROUP BY vacunas.especie,costo_visita
 
 
+-- 7) 
+-- Obtener el total de ventas en vacunas por cada especie y raza de animal. 
+-- Ordenar de mayor a menor.
+
+SELECT SUM(vacunas.coste) as "Total Venta",vacunas.especie, animales.raza  FROM fichas_vacunas
+LEFT JOIN fichas_medicas ON fichas_vacunas.ficha_medica = fichas_medicas.id_ficha_medica
+LEFT JOIN animales ON fichas_medicas.animal = animales.id_animal
+LEFT JOIN vacunas ON fichas_vacunas.vacuna = vacunas.id_vacuna
+GROUP BY vacunas.especie,animales.raza
+ORDER BY vacunas.especie, "Total Venta" DESC
+;  
+-- En otra query obtener el total de ventas de cada vacuna. Ordenar de mayor a menor.
+SELECT SUM(vacunas.coste) as "Total Venta",vacunas.nombre  FROM fichas_vacunas
+LEFT JOIN vacunas ON fichas_vacunas.vacuna = vacunas.id_vacuna
+GROUP BY vacunas.nombre
+ORDER BY "Total Venta" DESC
+;
+
+
+-- 8) 
+-- Obtener tasa de vacunación por especie y edad. 
+-- La tasa de vacunación es el porcentaje de cada grupo que tiene
+-- todas las vacunas al día.
+--    Mostrar:
+--    - Especie
+--    - Edad
+--    - "Tasa de vacunación"
+--    Este último valor debe figurar como un valor entre 0 y 100 con el símbolo "%" al final.
+SELECT animales.especie,animales.edad,(
+COUNT(CASE  
+    WHEN vacunas.duracion_meses <> 0 AND fichas_vacunas.fecha_aplicacion + vacunas.duracion_meses * INTERVAL '1 MONTH' > current_timestamp THEN 1
+    WHEN vacunas.duracion_meses = 0 THEN 1 
+    ELSE null 
+end  )  * 100 ) / COUNT(fichas_medicas.id_ficha_medica) || '%' as "Tasa de vacunación"
+FROM fichas_vacunas
+LEFT JOIN fichas_medicas ON fichas_vacunas.ficha_medica = fichas_medicas.id_ficha_medica
+LEFT JOIN animales ON fichas_medicas.animal = animales.id_animal
+LEFT JOIN vacunas ON fichas_vacunas.vacuna = vacunas.id_vacuna
+GROUP BY animales.especie, animales.edad
+ORDER BY animales.especie DESC
+;
+
+
